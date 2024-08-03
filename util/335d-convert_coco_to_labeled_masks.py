4e5833aff3aef8f718049a997a20e45dda4023c9
6510110513 <6510110513@psu.ac.th>
2024-08-03 18:56:19 +0700

    add_util

--- util/335d-convert_coco_to_labeled_masks.py
+++ util/335d-convert_coco_to_labeled_masks.py
@@ -31,6 +31,7 @@ import tifffile
 import os
 import shutil
 import matplotlib.pyplot as plt
+import tqdm
 
 colors = {1:[255,0,0],2:[0,255,0],3:[0,0,255]}
 
@@ -38,22 +39,16 @@ def create_mask(image_info, annotations, output_folder):
     # Create an empty mask as a numpy array
     mask_np = np.zeros((image_info['height'], image_info['width'],3), dtype=np.uint8)
 
-    # Counter for the object number
-    object_number = 200
-
     for ann in annotations:
         if ann['image_id'] == image_info['id']:
             object_color = colors[ann['category_id']]
             # Extract segmentation polygon
             for seg in ann['segmentation']:
-                # print("test")
-                # print(seg)
-                # Convert polygons to a binary mask and add it to the main mask
                 rr, cc = skimage.draw.polygon(seg[1::2], seg[0::2], mask_np.shape)
                 mask_np[rr, cc,:] = object_color
                 object_number += 1 #We are assigning each object a unique integer value (labeled mask)
 
-    # Save the numpy array as a TIFF using tifffile library
+    # Save the numpy array as a TIFF using tifffile library or rgb .jpg image using pyplot
     mask_path = os.path.join(output_folder, image_info['file_name'].replace('.jpg', '_mask.jpg'))
     plt.imsave(mask_path,mask_np)
     # tifffile.imsave(mask_path, mask_np)
@@ -75,7 +70,7 @@ def main(json_file, mask_output_folder, image_output_folder, original_image_dir)
     if not os.path.exists(image_output_folder):
         os.makedirs(image_output_folder)
 
-    for img in images:
+    for img in tqdm.tqdm(images):
         # Create the masks
         create_mask(img, annotations, mask_output_folder)
         
@@ -84,14 +79,14 @@ def main(json_file, mask_output_folder, image_output_folder, original_image_dir)
     
         new_image_path = os.path.join(image_output_folder, os.path.basename(original_image_path))
         shutil.copy2(original_image_path, new_image_path)
-        print(f"Copied original image to {new_image_path}")
+        # print(f"Copied original image to {new_image_path}")
 
 
 if __name__ == '__main__':
-    original_image_dir = '/home/athip/psu/3/ecosys/food/food_segmentation.v3i.coco-segmentation/test'  # Where your original images are stored
-    json_file = '/home/athip/psu/3/ecosys/food/food_segmentation.v3i.coco-segmentation/test/_annotations.coco.json'
-    mask_output_folder = 'val2/masks2'  # Modify this as needed. Using val2 so my data is not overwritten
-    image_output_folder = 'val2/images2'  # 
+    original_image_dir = '/home/athip/psu/3/ecosys/food_github/food_segmentation.v5i.coco-segmentation/train'  # Where your original images are stored
+    json_file = '/home/athip/psu/3/ecosys/food_github/food_segmentation.v5i.coco-segmentation/train/_annotations.coco.json'
+    mask_output_folder = '/home/athip/psu/3/ecosys/food_github/data/segmentation/train/masks'  # Modify this as needed. Using val2 so my data is not overwritten
+    image_output_folder = '/home/athip/psu/3/ecosys/food_github/data/segmentation/train/images'  # 
     main(json_file, mask_output_folder, image_output_folder, original_image_dir)
 
 
